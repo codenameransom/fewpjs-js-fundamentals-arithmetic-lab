f8c8eb448c7af7905381cb4799bae09ad478fe42
codenameransom <codejoshuaroque@gmail.com>
2021-03-24 01:21:51 -0400

    going ham tonight :)

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 7;
+const num4 = 28;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() + 1);
+const mod = num4 / num3;
+const max = Math.max(0,3,4,5,6,7,20);
